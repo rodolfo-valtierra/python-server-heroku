f1269aa6b7e81a13e46d4c220caaba6d2b50fc69
rodolfo-valtierra <rodolfoisaac.valtierradomiguez@gmail.com>
2021-08-17 10:28:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+pip 
+  gunicorn
+  flask
+
+run
+  flask run
+
+proctfile
+  web: gunicorn app:<name file.py init>
---
+++ hello.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask('hello')
+
+@app.route("/")
+def hello():
+    return "<p>Hello world</p>"
+
